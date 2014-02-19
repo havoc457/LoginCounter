a77a40ccb4d7b2f31870b58fef2304d52b6cf4e5
Samuel Razzell <samuelrazzell@comcast.net>
2014-02-19 10:41:51 -0800

    Fixed urls to have unittTests

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('',
 	url(r'^users/add', views.add, name='add'),
 	url(r'^users/login', views.login, name='login'),
 	url(r'^TESTAPI/resetFixture', views.reset, name='reset'),
-	url(r'^TESTAPI/unitTests', views.unit, name='unitTests'),
+	url(r'^TESTAPI/unitTests', views.unitTests, name='unitTests'),
 )
